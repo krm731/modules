614b2b53abd9c89eb5867e30d4a6e88e5b53af04
David butland <david@cap.a.ongcp.co>
2020-08-13 12:05:31 +0100

    Added database and KMS modules

---
+++ kms/key/main.tf
@@ -0,0 +1,15 @@
+resource "google_kms_crypto_key" "key" {
+  name            = var.name
+  key_ring        = var.keyring.id
+	rotation_period = var.rotation_period
+  purpose         = var.purpose
+
+	version_template {
+		algorithm        = var.algorithm
+		protection_level = var.protection_level
+	}
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
