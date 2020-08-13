798de045dcf4ac58c80dd8326392a4ab6cd765d4
Rethesh Nair <retnair@publicisgroupe.net>
2020-08-13 10:57:49 +0530

    adding the module for ssl policy

---
+++ network/lb-sslpolicy/input.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  type = string
+  description = "Name of the SSL policy"
+}
+
+variable "project_id" {
+  type = string
+  description = "id of the project"
+}
+
+variable "min_tls_version" {
+  type = string
+  description = " The minimum version of SSL protocol"
+  default = "TLS_1_2"
+}
+
+variable "profile" {
+  type = string
+  description = "specifies the set of SSL features that can be used by the load balancer when negotiating SSL with clients.If using CUSTOM, the set of SSL features to enable must be specified in the customFeatures"
+  default = "MODERN"
+}
+
+variable "custom_features" {
+  type = list(string)
+  description = "specifies the set of SSL features that can be used by the load balancer when negotiating SSL with clients"
+  default = []
+}
